76061b825f3a8cd717e21613e2a75321f2082263
jnsprnw <6gqrbj@posteo.org>
2023-03-22 11:45:42 +0100

    Fix try

--- src/routes/content.json/+server.js
+++ src/routes/content.json/+server.js
@@ -2,6 +2,7 @@ import events from '$events';
 
 export async function GET() {
   return new Response(JSON.stringify(events), {
+    status: 200,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
