28824cb8bbcb58380edf0be7ecfbc5ab8e49d421
Jonas Parnow <6gqrbj@posteo.org>
2023-12-12 11:01:21 +0100

    More info in header

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,5 @@
-export const title = 'Popsalon Archiv';
-export const description = 'Unvollständig und inoffiziell';
+export const title = 'Popsalon im Deutschen Theater Archiv';
+export const description =
+  'Popsalon im Deutschen Theater – Tobi Müller und Jens Balzer im Gespräch – Unvollständiges und inoffizielles Archiv';
 export const author = 'Jonas Parnow';
 export const baseURL = 'https://popsalon.jonasparnow.com/';
