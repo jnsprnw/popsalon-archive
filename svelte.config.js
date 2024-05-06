c06e458da7defd6b6fee9d7f1a353f046dc9a7f9
Jonas Parnow <6gqrbj@posteo.org>
2024-05-06 23:22:29 +0200

    Packages updated

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,22 @@
-import path from 'path';
-
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    alias: {
-      $events: path.resolve('./src/data/events.json'),
-    },
     prerender: {
       entries: ['/', '/sitemap.xml', '/content.json'],
     },
-  },
-  vite: {
-    define: {
-      global: {},
+    alias: {
+      $events: 'src/data/events.json',
     },
   },
 };
