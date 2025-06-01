16ab243c5d54df0029a183a712216c71affabd25
Jonas Parnow <6gqrbj@posteo.org>
2025-06-01 20:07:40 +0200

    Clean up

--- src/routes/[event]/+page.server.ts
+++ src/routes/[event]/+page.server.ts
@@ -7,75 +7,81 @@ import { error } from '@sveltejs/kit';
  * @returns Das Event-Objekt oder undefined wenn nicht gefunden
  */
 function getPostByEvent(slug: string) {
-  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+	const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
-  if (dateRegex.test(slug)) {
-    // Datumsformat YYYY-MM-DD wurde gefunden
-    const index = events.findIndex((event) => {
-      if (typeof event.date.iso !== 'string') return false;
-      // Beginnt die ISO-Datumszeichenfolge mit dem angegebenen Datum?
-      return event.date.iso.startsWith(slug);
-    });
-    return {
-      event: events[index],
-      index: index,
-      alternate: events[index].number,
-    };
-  }
+	if (dateRegex.test(slug)) {
+		// Datumsformat YYYY-MM-DD wurde gefunden
+		const index = events.findIndex((event) => {
+			if (typeof event.date.iso !== 'string') return false;
+			// Beginnt die ISO-Datumszeichenfolge mit dem angegebenen Datum?
+			return event.date.iso.startsWith(slug);
+		});
+		return {
+			event: events[index],
+			index: index,
+			alternate: events[index].number
+		};
+	}
 
-  // Versuche, ob slug eine Nummer ist
-  const eventNumber = Number.parseInt(slug, 10);
+	// Versuche, ob slug eine Nummer ist
+	const eventNumber = Number.parseInt(slug, 10);
 
-  if (!Number.isNaN(eventNumber)) {
-    // Suche nach Events mit der Nummer eventNumber
-    const index = events.findIndex((event) => event.number === eventNumber);
-    return {
-      event: events[index],
-      index: index,
-      alternate: events[index].date.iso.slice(0, 10),
-    };
-  }
+	if (!Number.isNaN(eventNumber)) {
+		// Suche nach Events mit der Nummer eventNumber
+		const index = events.findIndex((event) => event.number === eventNumber);
+		return {
+			event: events[index],
+			index: index,
+			alternate: events[index].date.iso.slice(0, 10)
+		};
+	}
 
-  return undefined;
+	return undefined;
 }
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-  const { event: eventSlug } = params;
-  const { event, index, alternate } = getPostByEvent(eventSlug);
-  if (!event) {
-    error(404, {
-      message: 'Not found',
-    });
-  }
+	const { event: eventSlug } = params;
+	const match = getPostByEvent(eventSlug);
+	if (!match) {
+		error(404, {
+			message: 'Not found'
+		});
+	}
+	const { event, index, alternate } = match;
+	if (!event) {
+		error(404, {
+			message: 'Not found'
+		});
+	}
 
-  const nextEvent = index > 0 ? events[index - 1] : null;
-  const previousEvent = index < events.length - 1 ? events[index + 1] : null;
+	const nextEvent = index > 0 ? events[index - 1] : null;
+	const previousEvent = index < events.length - 1 ? events[index + 1] : null;
 
-  return {
-    event,
-    previousEvent: previousEvent ? previousEvent.date.iso.slice(0, 10) : null,
-    nextEvent: nextEvent ? nextEvent.date.iso.slice(0, 10) : null,
-    alternate,
-    canonical: events[index].date.iso.slice(0, 10),
-  };
+	return {
+		event,
+		previousEvent: previousEvent ? previousEvent.date.iso.slice(0, 10) : null,
+		nextEvent: nextEvent ? nextEvent.date.iso.slice(0, 10) : null,
+		alternate,
+		canonical: events[index].date.iso.slice(0, 10)
+	};
 }
 
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
-  // Generiere URLs für alle Events basierend auf der Event-Nummer und dem Datum
-  const urls = [];
-  for (const event of events) {
-    if (typeof event.number === 'number') {
-      urls.push({
-        event: String(event.number),
-      });
-    }
-    urls.push({
-      event: event.date.iso.split('T')[0],
-    });
-  }
-  return urls;
+	// Generiere URLs für alle Events basierend auf der Event-Nummer und dem Datum
+	const urls = [];
+	for (const event of events) {
+		if (typeof event.number === 'number') {
+			urls.push({
+				event: String(event.number)
+			});
+		}
+		urls.push({
+			event: event.date.iso.split('T')[0]
+		});
+	}
+	return urls;
 }
 
 export const prerender = true;
